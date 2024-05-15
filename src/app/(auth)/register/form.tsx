dc9556840a766c3015cb5d5692dd977773b5ef1f
adii1203 <aditya32ft@gmail.com>
2024-05-15 22:57:25 +0530

    feat: Add user registration and erron landling

--- src/app/(auth)/register/form.tsx
+++ src/app/(auth)/register/form.tsx
@@ -1,27 +1,81 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "sonner";
 
 const RegisterForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const handelFormSubmit = async () => {
+    setLoading(true);
+    const res = await fetch("/api/user/register", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    if (data.success) {
+      toast.success(data.message);
+      setFormData({ name: "", email: "", password: "" });
+      setLoading(false);
+    } else {
+      setLoading(false);
+      toast.error(data.message);
+    }
+  };
   return (
-    <form className="space-y-4">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handelFormSubmit();
+      }}
+      className="space-y-4">
       <div className="space-y-2">
         <Label htmlFor="email">Name</Label>
-        <Input id="name" placeholder="name" required type="name" />
+        <Input
+          value={formData.name}
+          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+          id="name"
+          placeholder="name"
+          required
+          type="name"
+        />
       </div>
       <div className="space-y-2">
         <Label htmlFor="email">Email</Label>
-        <Input id="email" placeholder="m@example.com" required type="email" />
+        <Input
+          value={formData.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+          id="email"
+          placeholder="m@example.com"
+          required
+          type="email"
+        />
       </div>
       <div className="space-y-2">
         <div className="flex items-center justify-between">
           <Label htmlFor="password">Password</Label>
         </div>
-        <Input id="password" required type="password" />
+        <Input
+          value={formData.password}
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
+          id="password"
+          required
+          type="password"
+        />
       </div>
-      <Button className="w-full" type="submit">
+      <Button disabled={loading} className="w-full" type="submit">
         Sign up
       </Button>
     </form>
