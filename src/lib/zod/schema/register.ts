cf97520f09cbbc3df40ec71c968c1a77f680a25c
adii1203 <aditya32ft@gmail.com>
2024-05-16 13:06:55 +0530

    refactor: Update validation rules for name and password fields

--- src/lib/zod/schema/register.ts
+++ src/lib/zod/schema/register.ts
@@ -1,11 +1,14 @@
 import { z } from "zod";
 
 export const getRegisterSchema = z.object({
-  name: z.string({ message: "name is required" }).min(3).max(15),
+  name: z
+    .string()
+    .min(3, { message: "Name must cantain at least 3 charactor(s)" })
+    .max(15),
   email: z.string().email({
     message: "Please enter a valid email address",
   }),
   password: z
     .string()
-    .min(8, { message: "Password must be at least 8 characters long" }),
+    .min(6, { message: "Password must be at least 6 characters long" }),
 });
