65a097e147ec54c2e5c338a1781bc90cd98f6f81
adii1203 <aditya32ft@gmail.com>
2024-05-17 22:14:18 +0530

    refactor: Update routes.ts for improved authentication handling

---
+++ routes.ts
@@ -0,0 +1,24 @@
+/**
+ * The routes that are public and do not require authentication
+ * @type {string[]}
+ */
+export const publicRoutes = ["/"];
+
+/**
+ * An array of routes that are user for authentication
+ * These routes will redirect logged in users to /dashboard
+ * @type {string[]}
+ */
+export const authRoutes = [
+  "/login",
+  "/register",
+  "/forgot-password",
+  "/verify-email",
+];
+
+/**
+ * The prefix for the api auth routes
+ * Routes that starts with this prefix are for authentication
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
