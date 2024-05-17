dafa264e84e73408c7913d5ad4ef8b48840c3668
adii1203 <aditya32ft@gmail.com>
2024-05-17 22:15:01 +0530

    feat: Add login action for authentication

---
+++ src/actions/login.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import { signIn } from "@/auth";
+import { CredentialsSignin } from "next-auth";
+
+export const login = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) => {
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: "/dashboard",
+      redirect: true,
+    });
+  } catch (error) {
+    const err = error as CredentialsSignin;
+    return err.cause;
+  }
+};
