1b7bc27e28f6da478c4b7d5876e3fff8cd88ab3c
adii1203 <aditya32ft@gmail.com>
2024-05-19 00:57:25 +0530

    refactor: Add users table to database schema

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,29 +1,10 @@
-import mongoose from "mongoose";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
 
-type ConnectionObj = {
-  isConnected?: number;
-};
+const client = neon(process.env.DATABASE_URL!);
 
-const connection: ConnectionObj = {};
-
-const connectDB = async (): Promise<void> => {
-  if (connection.isConnected) {
-    console.log("Using existing database connection");
-    return;
-  }
-
-  try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI + "/" + process.env.DATABASE_NAME}` || ""
-    );
-    connection.isConnected = connectionInstance.connections[0].readyState;
-
-    console.log("Database connected");
-    // console.log(connectionInstance);
-  } catch (error) {
-    console.log("Database connection failed", error);
-    process.exit(1);
-  }
-};
-
-export default connectDB;
+export const db = drizzle(client, {
+  schema: schema,
+  logger: true,
+});
