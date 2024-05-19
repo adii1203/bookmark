63cd6fafba3e8c85ab6bc5fcd91938347a6c1439
adii1203 <aditya32ft@gmail.com>
2024-05-19 14:07:02 +0530

    refactor: Add "megan" font family to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        megan: ["var(--font-megan)"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
