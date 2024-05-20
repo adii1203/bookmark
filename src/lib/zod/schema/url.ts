873cddcdc8f70e2aef1ff4e9aeb3018c5ffa57ac
adii1203 <aditya32ft@gmail.com>
2024-05-21 00:02:49 +0530

    refactor: Add Url zod schema

--- src/lib/zod/schema/url.ts
+++ src/lib/zod/schema/url.ts
@@ -1,3 +1,8 @@
 import { z } from "zod";
 
 export const urlSchema = z.string().url({ message: "Invalid URL" });
+
+export const bookmarkSchema = z.object({
+  url: urlSchema,
+  userId: z.string().uuid({ message: "Invalid user ID" }),
+});
