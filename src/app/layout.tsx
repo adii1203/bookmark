91b76ec2d982ac5b38bffe37ffb6fdb546f1ac96
adii1203 <aditya32ft@gmail.com>
2024-05-22 15:53:53 +0530

    chore: Add QueryProvider component for managing queries with @tanstack/react-query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import QueryProvider from "@/tanstack/query-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,11 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Toaster />
-      </body>
-    </html>
+    <QueryProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          {children}
+          <Toaster />
+        </body>
+      </html>
+    </QueryProvider>
   );
 }
