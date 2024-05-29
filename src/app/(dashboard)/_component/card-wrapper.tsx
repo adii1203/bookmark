163d7d12eac0425fe9f06a432f8a931ee0a7d687
adii1203 <aditya32ft@gmail.com>
2024-05-29 13:16:59 +0530

    refactor: Add loading state to CardWrapper component

--- src/app/(dashboard)/_component/card-wrapper.tsx
+++ src/app/(dashboard)/_component/card-wrapper.tsx
@@ -5,6 +5,7 @@ import Card from "./card";
 import { useQuery } from "@tanstack/react-query";
 import { getBookmarks } from "@/actions/get-bookmark";
 import { BookmarkType } from "@/utils/types";
+import CardLoading from "@/ui/dashboard/card-loading";
 
 const CardWrapper = () => {
   const { data, isError, isPending } = useQuery({
@@ -12,13 +13,17 @@ const CardWrapper = () => {
     queryFn: () => getBookmarks(),
   });
 
-  return (
-    <div className="space-y-4 max-w-[60rem] mx-auto">
-      {(data as BookmarkType[])?.map((bookmark: BookmarkType) => {
-        return <Card key={bookmark.id} data={bookmark} />;
-      })}
-    </div>
-  );
+  if (isPending) {
+    return <CardLoading />;
+  }
+  if (data)
+    return (
+      <div className="space-y-4 max-w-[60rem] mx-auto">
+        {(data as BookmarkType[])?.map((bookmark: BookmarkType) => {
+          return <Card key={bookmark.id} data={bookmark} />;
+        })}
+      </div>
+    );
 };
 
 export default CardWrapper;
---
+++ src/ui/dashboard/card-loading.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const CardLoading = () => {
+  return (
+    <div className="space-y-8">
+      <div className="animate-pulse flex space-x-4 max-w-[30rem] mx-auto">
+        <div className="flex-1 space-y-4 py-1">
+          <div className="h-4 bg-gray-200 rounded w-3/4"></div>
+          <div className="space-y-2">
+            <div className="h-4 bg-gray-200 rounded"></div>
+            <div className="h-4 bg-gray-200 rounded w-5/6"></div>
+          </div>
+        </div>
+      </div>
+      <div className="animate-pulse flex space-x-4 max-w-[30rem] mx-auto">
+        <div className="flex-1 space-y-4 py-1">
+          <div className="h-4 bg-gray-200 rounded w-3/4"></div>
+          <div className="space-y-2">
+            <div className="h-4 bg-gray-200 rounded"></div>
+            <div className="h-4 bg-gray-200 rounded w-5/6"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardLoading;
